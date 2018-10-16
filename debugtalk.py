70a491aa97c6beab045c796243408c3cbe5d7eaa
sumeilan <912041511@qq.com>
2018-10-16 18:59:48 +0800

    10.16

---
+++ debugtalk.py
@@ -0,0 +1,13 @@
+global val  # 在使用前初次声明
+val = ''  # 给全局变量赋值
+
+
+def save_token(token):
+    global val  # 再次声明，表示在这里使用的是全局变量，而不是局部变量
+    val = token  # 这样的话全局变量也会为5
+    print(val)
+
+def get_token():
+    global val
+    print(val)
+
